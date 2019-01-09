60e7c198e073b9d5344f96c0f8c83e087e953393
viviarui <zr1450995198@163.com>
2019-01-09 10:48:44 +0800

    添加国际化配置

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import { LocaleProvider } from 'antd';
+import zh_CN from 'antd/lib/locale-provider/zh_CN';
+import 'moment/locale/zh-cn';
 import thunk from 'redux-thunk';
 import history from './utils/history';
 import storeTree from './store';
@@ -11,7 +14,9 @@ const store = createStore(storeTree, applyMiddleware(thunk));
 
 ReactDom.render(
     <Provider store={store}>
-        <App history={history}/>
+        <LocaleProvider locale={zh_CN}>
+            <App history={history}/>
+        </LocaleProvider>
     </Provider>,
     document.getElementById('app')
 )
