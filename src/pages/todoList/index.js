4da0f659bc1a8d45e48aa18e54ead2ad3f1f28d1
viviarui <zr1450995198@163.com>
2019-01-10 11:32:03 +0800

    refactor: 值班周期表优化

--- src/pages/todoList/index.js
+++ src/pages/todoList/index.js
@@ -1,5 +1,5 @@
 import React, { PureComponent } from 'react';
-import { Button, Calendar, Badge } from 'antd';
+import { Button, Calendar, Badge, DatePicker } from 'antd';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import moment from 'moment';
@@ -22,40 +22,22 @@ class TodoList extends PureComponent {
     };
     constructor(props){
         super(props);
+        this.state = {
+            changeDate: '2019-01-01'
+        }
     }
     componentDidMount(){
         this.props.getTodoList();
     }
     dateCellRender = (date) => {
         let value = '';
-        switch(date.dayOfYear() % 8) {
-            case 1:
-                value = '早班';
-                break;
-            case 2:
-                value = '早班';
-                break;
-            case 3:
-                value = '中班';
-                break;
-            case 4:
-                value = '中班';
-                break;
-            case 5:
-                value = '晚班';
-                break;
-            case 6:
-                value = '晚班';
-                break;
-            case 7:
-                value = '休息'
-                break;
-            case 0:
-                value = '休息'
-                break;
-            default:
-
-        }
+        // 获取现在时间距入职时间多少天
+        let dBack = (moment(date.format('YYYY-MM-DD')).unix() - moment(this.state.changeDate).unix()) / 3600 / 24;
+        // 过去时间，直接返回
+        if(dBack < 0) return;
+        // 使用数组映射 避免swtich case 判断
+        let toDo = ['早', '早', '中', '中', '晚', '晚', '休', '休'];
+        value = toDo[dBack % 8];
         return (
             <Badge status='warning' text={value} />
         );
@@ -65,11 +47,15 @@ class TodoList extends PureComponent {
         // 上班周期为早早，中中，晚晚，休休，8天一个周期
         return (
             <div>
+                你多少号入职的呢？
+                <DatePicker 
+                    disabledDate={ (currentDate) => currentDate > moment() } 
+                    onChange={(value) =>{ this.setState({ changeDate: value.format('YYYY-MM-DD') }) }}
+                />
                 <Calendar 
-                    dateCellRender={this.dateCellRender}
-                    validRange={[moment('2019-01-01'), moment('2019-12-31')]}
+                    dateCellRender={ this.dateCellRender }
                 ></Calendar>
-                <Button onClick={()=> {this.props.history.push('./todoDetail')}}>跳转详情</Button>
+                <Button onClick={()=> { this.props.history.push('./todoDetail')} }>跳转详情</Button>
             </div>
         )
     }
