9d1cc997b8d14d530bac21b8367f025a2abf6110
viviarui <zr1450995198@163.com>
2019-11-11 10:57:43 +0800

    feat: use hooks

--- src/components/CitySelector/index.tsx
+++ src/components/CitySelector/index.tsx
@@ -16,74 +16,62 @@ interface State {
 }
 
 const { Option } = Select;
-class Component extends React.PureComponent<Props, State> {
-    constructor(props:any) {
-      super(props);
-      this.state = {
-        visible: false,
-        data: []
-      }
-    }
 
-    componentDidMount() {
-      this.fetchData();
-    }
+const Component: React.SFC<Props> = (props) => {
+  const { length, mode, value, onChange } = props;
 
-    fetchData = () => {
-      // const res = await request('/api/businesstrip/cities');
-      this.setState({ data: cities || [] });
-    };
+  const [visible, setVisible] = React.useState(false);
+  const [data, setData] = React.useState()
 
-    openModal = () => {
-      this.setState({ visible: true  });
-    }
+  const fetchData = () => {
+    // const res = await request('/api/businesstrip/cities');
+    setData(cities)
+  };
 
-    handleChangeVisible = () => {
-      this.setState({ visible: false })
-    }
+  React.useEffect(fetchData, [])
 
-    onOk = (v: any) => {
-      const { onChange, mode } = this.props;
-      const { data } = this.state;
-      const o:any = [];
-      mode == 'multiple' && v && v.forEach((cityId:any) => {
-        data.forEach((item:any) => {
-          item.cityId == cityId && o.push(item)
-        })
+  const openModal = () => {
+    setVisible(true)
+  }
+
+  const handleChangeVisible = () => {
+    setVisible(false)
+  }
+
+  const onOk = (v: any) => {
+    const o:any = [];
+    mode == 'multiple' && v && v.forEach((cityId:any) => {
+      data.forEach((item:any) => {
+        item.cityId == cityId && o.push(item)
       })
-      this.handleChangeVisible();
-      onChange && onChange(v, o);
-    }
+    })
+    handleChangeVisible();
+    onChange && onChange(v, o);
+  }
 
-    render() {
-      const { length, mode, value } = this.props;
-      const { visible, data } = this.state;
-      return (
-        <div>
-          <Select
-            {...this.props}
-            style={{ width: 300 }}
-            open={false}
-            onDropdownVisibleChange={this.openModal}
-            getPopupContainer={ (triggerNode:any) => {return triggerNode.parentNode} }
-          >
-            {
-              data.map((i:any) => <Option value={i.cityId} title={i.cityName}>{i.cityName}</Option>)
-            }
-          </Select>
-          <CheckModal
-            visible={visible}
-            value={value}
-            handleChangeVisible={this.handleChangeVisible}
-            onOk={this.onOk}
-            data={data}
-            length={length}
-            multiple={mode=='multiple'}
-          />
-        </div>
-      )
-    }
-
+  return (
+    <div>
+      <Select
+        {...props}
+        style={{ width: 300 }}
+        open={false}
+        onDropdownVisibleChange={openModal}
+        getPopupContainer={ (triggerNode:any) => {return triggerNode.parentNode} }
+      >
+        {
+          data.map((i:any) => <Option value={i.cityId} title={i.cityName}>{i.cityName}</Option>)
+        }
+      </Select>
+      <CheckModal
+        visible={visible}
+        value={value}
+        handleChangeVisible={handleChangeVisible}
+        onOk={onOk}
+        data={data}
+        length={length}
+        multiple={mode=='multiple'}
+      />
+    </div>
+  )
 }
-
 export default Form.create<Props>()(Component);
